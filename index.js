aa5b7970a09da967e9b26182dd1efac5d9bf8938
Jerry Brown <brownjer3@gmail.com>
2018-03-04 16:58:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   var rank = document.getElementById("app").querySelectorAll("ul.ranked-list li");
   
   for (var i = 0; i < rank.length; i++) {
-    rank[i].innerHTML = (rank[i].innerHTML + n).toString();
+    rank[i].innerHTML = parseInt((rank[i].innerHTML + n).toString());
     
   }
 }
