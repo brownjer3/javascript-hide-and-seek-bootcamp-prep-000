29a92da848beb02309b2341d1ec25f35acda3870
Jerry Brown <brownjer3@gmail.com>
2018-03-04 17:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   var deepDivs = document.getElementById("app").querySelectorAll("div#grand-node div");
-  
+  return deepDivs;
 
   //for (var i = 0; i < deepDivs.length; i++) {
     
